042425e8a402766817419207805b4a6fb3e8b239
Evelyn Zepeda <evaynnn@gmail.com>
2024-02-11 18:12:07 -0800

    Week 5

---
+++ week-5/zepeda-hobbies.py
@@ -0,0 +1,36 @@
+#
+#	Title: zepeda-projections.js
+#    Author: Professor Krasso
+#    Date: 2/10/24
+#    Description: MongoDB Projections
+ 
+
+#Creating a variable that holds the hobbies in an array
+hobbies = ["Pastel Painting", "Gaming", "Traveling", "Hiking", "Gouache Painting"]
+
+#A for loop that loops through the hobbies and prints them to the console
+for hobby in hobbies:
+    print(hobbies)
+
+#Creating a variable that holds an array with the days of the week
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+#Creating a for loop that iterates over the list of days
+for day in days:
+    if day == "Sunday":
+        print("Take the day off and enjoy your hobbies.")
+    elif day == "Monday":
+        print("This is a work day.")
+    elif day == "Tuesday":
+        print("This is a workday.")
+    elif day == "Wednesday":
+        print("This is a work day.")
+    elif day == "Thursday":
+        print("This is a workday.")
+    elif day == "Friday":
+        print("This is a workday.")
+    elif day == "Saturday":
+        print("Take the day off and enjoy your hobbies.")
+    else:
+        print(day)
+        
